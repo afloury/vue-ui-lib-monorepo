c2652f1e384979a4ce075c787c1ae3e4b2beb260
Antoine FLOURY <floury.antoine@gmail.com>
2025-04-28 23:10:10 +1000

    Simplify the web app example + improve the custom component

--- packages/ui-library/src/components/MyComp.vue
+++ packages/ui-library/src/components/MyComp.vue
@@ -1,5 +1,16 @@
 <template>
-  <div style="background-color: blue">My Component</div>
+  <div
+    style="
+      background-color: blue;
+      color: white;
+      width: fit-content;
+      padding: 1rem;
+      border-radius: 7px;
+      opacity: 0.7;
+    "
+  >
+    My Component
+  </div>
 </template>
 <script setup lang="ts"></script>
 <style scoped></style>
